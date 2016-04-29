0fef586b8ffc673c78355bda937dc2ecc760f2b3
Alex Torres <alexst07@gmail.com>
2016-04-29 17:29:58 -0300

    Change data interface

--- erised/core/src/data_base.h
+++ erised/core/src/data_base.h
@@ -6,12 +6,35 @@
 
 namespace erised {
 
+template<size_t N>
+class Pos {
+};
+
+template<>
+class Pos<2> {
+ public:
+  Pos(size_t x, size_t y)
+    : x_(x)
+    , y_(x) {}
+
+  size_t X() const {
+    return x_;
+  }
+
+  size_t Y() const {
+    return y_;
+  }
+
+ private:
+  size_t x_, y_;
+};
+
 /**
  *
  *
  */
-template< typename T, size_t N>
-class DataBase {
+template<typename T, size_t N>
+class MatBase {
  public:
   static constexpr size_t order = N;
   using value_type = T;
@@ -19,24 +42,27 @@ class DataBase {
   MatBase() = default;
   virtual ~MatBase() = default;
 
-  template<class Fn = T(T)>
-  virtual void Apply(Fn&& fn) = 0;
+  template<class Fn = void(T)>
+  virtual void Map(Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  virtual void RowApply(Fn&& fn) = 0;
+  template<class Fn = void(T)>
+  virtual void RowMap(size_t i, Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  virtual void ColApply(Fn&& fn) = 0;
+  template<class Fn = void(T)>
+  virtual void ColMap(size_t i, Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
+  template<class Fn = T(T,T)>
   virtual T Reduce(Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  virtual T RowReduce(Fn&& fn) = 0;
+  template<class Fn = T(T,T)>
+  virtual T RowReduce(size_t i, Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  virtual T ColReduce(Fn&& fn) = 0;
+  template<class Fn = T(T,T)>
+  virtual T ColReduce(size_t i, Fn&& fn) = 0;
 };
+
+template<typename T>
+typedef MatBase<T, 2> DataBase;
 
 }
 #endif //__ERISED_MATRIX_BASE__
--- erised/core/src/data_csr.h
+++ erised/core/src/data_csr.h
@@ -13,38 +13,42 @@ namespace erised {
 class CsrRow;
 
 template<typename T>
-class DataCsr: public DataBase<T, 2> {
+class DataCsr: public DataBase {
  public:
   using size_type = std::size_t;
 
-//   CsrMat() {}
-//   CsrMat(size_type rows, size_type cols): rows_{rows}, cols_{cols} {}
-//   CsrMat(const CsrMat<T>& m): rows_{m.rows_}, cols_{m.cols_},
-//       elems_{m.elems_}, size_cols_{m.size_cols_} {}
-//
-//   CsrMat(CsrMat<T>&& m): rows_{std::move(m.rows_)}, cols_{std::move(m.cols_)},
-//       elems_{std::move(m.elems_)} {}
+  CsrMat();
+  CsrMat(size_type rows, size_type cols);
+  CsrMat(std::initializer_list<std::initializer_list<T>> set);
+  CsrMat(const CsrMat<T>& m);
+  CsrMat(CsrMat<T>&& m);
 
   CsrMat();
   CsrMat(size_type rows, size_type cols);
 
-  template<class Fn = T(T)>
-  void Apply(Fn&& fn) override;
+  template<class Fn = void(T)>
+  virtual void Map(Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  void RowApply(Fn&& fn) override;
+  template<class Fn = void(T)>
+  virtual void RowMap(size_t i, Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  void ColApply(Fn&& fn) override;
+  template<class Fn = void(T)>
+  virtual void ColMap(size_t i, Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  T Reduce(Fn&& fn) override;
+  template<class Fn = T(T,T)>
+  virtual T Reduce(Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  T RowReduce(Fn&& fn) override;
+  template<class Fn = T(T,T)>
+  virtual T RowReduce(size_t i, Fn&& fn) = 0;
 
-  template<class Fn = T(T)>
-  T ColReduce(Fn&& fn) override;
+  template<class Fn = T(T,T)>
+  virtual T ColReduce(size_t i, Fn&& fn) = 0;
+
+  void AddRow(const std::vector<T>& row);
+  void AddRow(const T* row, size_type size);
+
+  void AddCol(const std::vector<T>& col);
+  void AddCol(const T* col, size_type size);
 
  private:
   std::vector<size_type> rows_;
