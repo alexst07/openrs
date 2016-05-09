6707a8df370e8337bf91b3d6cd063a0f86976e7b
Alex Torres <alexst07@gmail.com>
2016-05-09 15:40:06 -0300

    Adds cuda sparse matrix class declaration

---
+++ erised/core/src/cuda_csr.h
@@ -0,0 +1,71 @@
+#pragma once
+#define ERISED_CSR_MATRIX_H_
+
+#include "data_base.h"
+
+#include <initializer_list>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <iostream>
+
+// namespace erised::cuda
+namespace erised { namespace cuda {
+
+template<typename T>
+class GpuCsr {
+ public:
+  using size_type = std::size_t;
+  using ElemIter = typename std::vector<T>::iterator;
+  using ConstElemIter = typename std::vector<T>::const_iterator;
+
+  static const int INVALID_LINE;
+
+  GpuCsr();
+  GpuCsr(size_type rows, size_type cols);
+  GpuCsr(std::initializer_list<std::initializer_list<T>> set);
+  GpuCsr(const GpuCsr<T>& m);
+  GpuCsr(GpuCsr<T>&& m);
+
+  GpuCsr<T>& operator=(const GpuCsr<T>& m);
+  GpuCsr<T>& operator=(GpuCsr<T>&& m);
+
+  template<typename MapFn>
+  void Map(MapFn&& fn) override;
+
+  template<typename MapFn>
+  void RowMap(size_t i, MapFn&& fn) override;
+
+  template<typename MapFn>
+  void ColMap(size_t i, MapFn&& fn) override;
+
+  template<typename ReduceFn>
+  T Reduce(ReduceFn&& fn) override;
+
+  template<typename ReduceFn>
+  T RowReduce(size_t i, ReduceFn&& fn) override;
+
+  template<typename ReduceFn>
+  T ColReduce(size_t i, ReduceFn&& fn) override;
+
+  void AddRow(const std::vector<T>& row);
+  void AddRow(const T* row, size_type size);
+
+  void AddCol(const std::vector<T>& col);
+  void AddCol(const T* col, size_type size);
+
+  template<typename U>
+  friend std::ostream& operator<<(std::ostream& stream, const GpuCsr<U>& mat);
+
+ private:
+  int* rows_offset_;
+  size_type* cols_index_;
+  std::vector<T> elems_;
+
+  size_type size_rows_;
+  size_type size_cols_;
+};
+
+}}
+
+#include <data_csr-inl.h>
