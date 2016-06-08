e9cbd51d0e4c98aa35a6c506c49971e23bc0855e
Alex Torres <alexst07@gmail.com>
2016-06-08 14:04:25 -0300

    Implements normalization functions

--- erised/core/src/basic_statistic.h
+++ erised/core/src/basic_statistic.h
@@ -87,9 +87,77 @@ T Variance(const Data<T>& data, size_t i, Axis axis) {
 }
 
 template<class T, template<typename> class Data>
+T Variance(const Data<T>& data, size_t i, Axis axis, size_t n) {
+  T u = internal::Avarage(data, i, axis, n);
+  T x2;
+
+  if (axis == Axis::ROW) {
+    x2 = data.RowReduce([](float a, float b) {
+      return a*a + b;
+    });
+  } else {
+    x2 = data.ColReduce([](float a, float b) {
+      return a*a + b;
+    });
+  }
+
+  T res = x2/n - u*u;
+  return res;
+}
+
+template<class T, template<typename> class Data>
 T StandardDeviation(const Data<T>& data, size_t i, Axis axis) {
   T variance = Variance(data, i, axis);
   return std::sqrt(variance);
+}
+
+template<class T, template<typename> class Data>
+T StandardDeviation(const Data<T>& data, size_t i, Axis axis, size_t n) {
+  T variance = Variance(data, i, axis, n);
+  return std::sqrt(variance);
+}
+
+template<class T, template<typename> class Data>
+void Standardization(Data<T>* data, size_t i, Axis axis, size_t n) {
+  T u = Avarage(*data, i, axis, n);
+  T a = StandardDeviation(*data, i, axis, n);
+
+  if (axis == Axis::ROW) {
+    data->RowMap(i, [u, a](T x) -> T { return (x - u)/a; });
+  } else {
+    data->ColMap(i, [u, a](T x) -> T { return (x - u)/a; });
+  }
+}
+
+template<class T, template<typename> class Data>
+void Standardization(Data<T>* data, size_t i, Axis axis) {
+  size_t n;
+
+  if (axis == Axis::ROW) {
+    // Count the number of elements on row i
+    n = data->NumElementsLine(i);
+  } else {
+    // Count the number of elements on col i
+    n = data->NumElementsCol(i);
+  }
+
+  Standardization(data, i, axis, n);
+}
+
+template<class T, template<typename> class Data>
+void Rescaling(Data<T>* data, size_t i, Axis axis, size_t n) {
+  T min = data->Min(i, axis);
+  T max = data->Max(i, axis);
+
+  T den = max - min;
+  if (den == 0)
+    throw std::overflow_error("Divide by zero exception");
+
+  if (axis == Axis::ROW) {
+    data->RowMap(i, [den, min, max](T x) -> T { return (x - min)/den; });
+  } else {
+    data->ColMap(i, [den, min, max](T x) -> T { return (x - min)/den; });
+  }
 }
 
 }
