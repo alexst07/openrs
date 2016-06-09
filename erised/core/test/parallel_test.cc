6fee74413a976bd5c3be93ecd212c344142a699d
Alex Torres <alexst07@gmail.com>
2016-06-08 23:49:53 -0300

    Adds more tests for parallel

--- erised/core/test/parallel_test.cc
+++ erised/core/test/parallel_test.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
+#include <atomic>
 #include <numeric>
 
 TEST(Parallel, parallel_for) {
@@ -34,4 +35,41 @@ TEST(Parallel, parallel_reduce) {
       }, std::plus<int>());
 
   std::cout << "total: " << r << "\n";
+}
+
+TEST(Parallel, parallel_for_sum_int) {
+  using namespace erised;
+
+  std::vector<int> v = {1, 2, 3, 4, 5, 6};
+  std::atomic<int> sum(0);
+
+  Range<int> range(0, 6);
+  parallel_for(range, [&](const Range<int>& r) {
+    for(int i=r.begin(); i!=r.end(); ++i )
+      sum += v[i];
+  });
+
+  std::cout << "total--: " << sum << "\n";
+}
+
+TEST(Parallel, parallel_reduce_int) {
+  using namespace erised;
+
+  std::vector<int> v = {1, 2, 3, 4, 5, 6};
+  std::atomic<int> sum(0);
+
+  Range<int> range(0, 6);
+  int r = parallel_reduce(range, static_cast<int>(0),
+      [&](const Range<int>& r, int value)->int {
+        int ret = value;
+        for(auto i = r.begin(); i!=r.end(); ++i) {
+          ret =  v[i] + ret;
+        }
+
+        return ret;
+      }, [](int a, int b) -> int {
+        return a + b;
+      });
+
+  std::cout << "total--: " << r << "\n";
 }
