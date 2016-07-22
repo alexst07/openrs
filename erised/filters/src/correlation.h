91378caceeb8a0b688deeaa839d5865b9b7cc99f
Alex Torres <alexst07@gmail.com>
2016-07-22 14:17:23 -0300

    Implement similarity for two elements

--- erised/filters/src/correlation.h
+++ erised/filters/src/correlation.h
@@ -8,72 +8,107 @@
 #include <memory>
 
 #include "exception.h"
+#include "data_base.h"
+#include "basic_statistic.h"
 
 namespace erised {
 
-template<
-  class TD,
-  class TS,
-  class Alloc,
-  template<typename, typename> class Data,
-  template <typename, typename> class Sim>
+template<class Data, class Sim>
 class Correlation {
  public:
-  virtual void Fit(Data<TD, Alloc> &data) = 0;
-  virtual const Sim<TD, Alloc>& Similarity() const noexcept = 0;
-  virtual Sim<TS, Alloc>& Similarity() noexcept = 0;
+  virtual void Fit(Data &data) = 0;
+  virtual const Sim& Similarity() const noexcept = 0;
+  virtual Sim& Similarity() noexcept = 0;
 
-  virtual typename Sim<TS, Alloc>::Slice Predict(size_t i) = 0;
+//   virtual typename Sim<T, Alloc>::Slice Predict(size_t i) = 0;
+
+ protected:
+  Correlation(Axis axis):axis_(axis) {}
+
+  Axis axis_;
 };
 
-template<
-  class TD,
-  class TS,
-  class Alloc,
-  template<typename, typename> class Data,
-  template <typename, typename> class Sim>
-class AdjustedCosine: public Correlation<TD, TS, Alloc, Data, Sim> {
+template<class Data, class Sim>
+class AdjustedCosine: public Correlation<Data, Sim> {
+  using Base = Correlation<Data, Sim>;
+  using value_type = typename Data::value_type;
+
  public:
-  virtual void Fit(Data<TD, Alloc> &data) {
+  AdjustedCosine(Axis axis): Base(axis) {}
+
+  void Fit(Data &data) override {
+    auto vec_rows = erised::Avarage(data, Base::axis_);
+    size_t data_size = Base::axis_ == Axis::ROW?
+        data.SizeRows(): data.SizeRows();
+
+//     Sim<T, Alloc> sim(data_size);
+
+    for (size_t i = 0; i < data_size; i++) {
+//       sim[i] =
+    }
+  }
+
+  const Sim& Similarity() const noexcept override {
 
   }
 
-  virtual const Sim<TD, Alloc>& Similarity() const noexcept {
+  Sim& Similarity() noexcept override {
 
   }
 
-  virtual Sim<TS, Alloc>& Similarity() noexcept {
+//   typename Sim<T, Alloc>::Slice Predict(size_t i) override {
+//
+//   }
 
+  std::array<value_type,3> SimTerms(const Data &data, size_t i, size_t j,
+                           value_type ia, value_type ja) {
+    auto l = [ia, ja](value_type v1, value_type v2, std::array<value_type,3> ret) {
+      std::array<float,3> r;
+      r[0] = (v1 - ia)*(v2 - ja);
+
+      value_type term = v1 - ia;
+      r[1] = term*term;
+
+      term = v2 - ja;
+      r[2] = term*term;
+
+      r[0] += ret[0];
+      r[1] += ret[1];
+      r[2] += ret[2];
+
+      return r;
+    };
+
+    std::array<value_type,3> arr = data.template ReduceCols<3>(i, j, l);
+    return arr;
   }
 
-  virtual typename Sim<TS, Alloc>::Slice Predict(size_t i) {
+ private:
 
-  }
 };
 
-template<
-  class TD,
-  class TS,
-  class Alloc,
-  template<typename, typename> class Data,
-  template <typename, typename> class Sim>
-class Pearson: public Correlation<TD, TS, Alloc, Data, Sim> {
- public:
-  virtual void Fit(Data<TD, Alloc> &data) {
-
-  }
-
-  virtual const Sim<TD, Alloc>& Similarity() const noexcept {
-
-  }
-
-  virtual Sim<TS, Alloc>& Similarity() noexcept {
-
-  }
-
-  virtual typename Sim<TS, Alloc>::Slice Predict(size_t i) {
-
-  }
-};
+// template<
+//   class T,
+//   class Alloc,
+//   template<typename, typename> class Data,
+//   template <typename, typename> class Sim>
+// class Pearson: public Correlation<T, Alloc, Data, Sim> {
+//  public:
+//   virtual void Fit(Data<T, Alloc> &data) {
+//
+//   }
+//
+//   virtual const Sim<T, Alloc>& Similarity() const noexcept {
+//
+//   }
+//
+//   virtual Sim<T, Alloc>& Similarity() noexcept {
+//
+//   }
+//
+// //   virtual typename Sim<T, Alloc>::Slice Predict(size_t i) {
+// //
+// //   }
+// };
 
 }
--- erised/filters/test/correlation_test.cc
+++ erised/filters/test/correlation_test.cc
@@ -1,11 +1,52 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <cmath>
 
 #include "correlation.h"
+#include "flann.h"
+#include "data_csr_map.h"
 
-TEST(Sample, Sampleunit) {
+template <class Data>
+class CorrelationTest : public ::testing::Test {
+ public:
+  Data mat_;
 
+  void SetUp() {
+    mat_ = Data{{5, 3, 4, 4, 0},
+                {3, 1, 2, 3, 3},
+                {4, 3, 4, 3, 5},
+                {3, 3, 1, 5, 4},
+                {1, 5, 5, 2, 1}};
+  }
+
+  void TearDown() {
+
+  }
+
+};
+
+TYPED_TEST_CASE_P(CorrelationTest);
+
+TYPED_TEST_P(CorrelationTest, AdjustedCosine) {
+  erised::AdjustedCosine<
+      erised::DataCsrMap<float>,
+      erised::flann::SimMat<float>> adcos(erised::Axis::COL);
+
+  erised::DataCsrMap<float> mat1 = this->mat_;
+
+  auto arr = adcos.SimTerms(mat1, 0, 4, 3.2, 3.25);
+
+  float res = arr[0]/(sqrt(arr[1])*sqrt(arr[2]));
+  std::cout << res;
 }
+
+
+REGISTER_TYPED_TEST_CASE_P(CorrelationTest,
+                           AdjustedCosine
+                          );
+
+typedef ::testing::Types<erised::DataCsrMap<float>> Types;
+INSTANTIATE_TYPED_TEST_CASE_P(My, CorrelationTest, Types);
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
