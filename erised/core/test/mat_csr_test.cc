dbeb11c90ce155b09982304ad29a96d43389134f
Alex Torres <alexst07@gmail.com>
2016-08-26 18:01:14 -0300

    Basic test for mat_csr

---
+++ erised/core/test/mat_csr_test.cc
@@ -0,0 +1,22 @@
+#include "mat_csr.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+// TEST(Sample, Sampleunit) {
+//   using namespace erised;
+//
+//   DataCsr<float> data{{0.44, 0.8, 0,   0,   0,   0,   0,   0,   0  },
+//                       {0,    0.8, 1.4, 0,   0,   0,   0,   0,   0  },
+//                       {0.22, 0,   0,   0.7, 0.4, 0,   0,   0,   0  },
+//                       {0.22, 0,   0,   0,   0,   0.7, 0.7, 0.7, 0  },
+//                       {0,    0,   0,   0,   0.4, 0,   0,   0,   0.7},
+//                       {0.22, 0,   0,   0,   0.4, 0,   0,   0,   0  }};
+//
+//   std::cout << data;
+// }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
